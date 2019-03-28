235611a7bfd59d157f4dd5514a021fafee641b3e
Wahe3bru <wahe3bru@gmail.com>
2019-03-28 11:03:41 +0200

    adding kpi cards

--- proto_dash/app.py
+++ proto_dash/app.py
@@ -2,7 +2,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-external_stylesheets = ["https://codepen.io/wahe3bru/pen/jJjwvB.css"]
+external_stylesheets = ["https://codepen.io/wahe3bru/pen/jJjwvB.css",
+    'https://unpkg.com/picnic',
+    "https://fonts.googleapis.com/css?family=Permanent+Marker"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -10,7 +12,27 @@ app.layout = html.Div(children=[
     html.Div(["logo"], className="box a", id="logo"),
     html.Div(["nav-bar"], className="box b", id="nav"),
     html.Div(["kpi-home"], className="box c", id="kpi-home"),
-    html.Div(["kpi api"], className="box d", id="kpi-api"),
+
+    # 2 cards
+    html.Div(
+    children=[html.Div(className="flex two", children=[
+        html.Div([
+            html.Article(className="card", children=[
+                html.Div(className="", children=['Current Temperature Outside']),
+                html.Div(className="footer", children=["26"], style={'font-family': 'Permanent Marker', 'textAlign': 'center', 'font-size':'2.5em'}),
+                html.Div(className="", children=['OpenWeatherMap']),
+            ], style={'backgroundColor': 'black'})
+        ]),
+        html.Div([
+            html.Article(className="card", children=[
+                html.Div(className="", children=['Logged today at:']),
+                html.Div(className="", children=['10:30'], style={'font-family': 'Permanent Marker', 'textAlign': 'center', 'font-size':'2.5em'}),
+                html.Div(className="footer", children=['28-03-2019']),
+            ], style={'backgroundColor': 'black'})
+        ]),
+    ]),
+    ], id="kpi-api", className="box d"),
+
     html.Div(["weather pic"], className="box pic", id="weather-pic"),
     html.Div(["dilly graph"], className="box e graph1", id="daily-graph"),
     html.Div(["Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."], className="box f", id="text"),
