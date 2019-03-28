d1027a367317cb1ed3fb58f42353ad97382b91a3
Wahe3bru <wahe3bru@gmail.com>
2019-03-28 15:43:11 +0200

    dropdown for logs graph

--- lessons/01-callbacks.py
+++ lessons/01-callbacks.py
@@ -1,4 +1,5 @@
 import dash
+import datetime
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
@@ -7,12 +8,22 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+today = datetime.datetime.strptime("2018-02-28 21:46", '%Y-%m-%d %H:%M')
+available_indicators = {"last 1": (today - datetime.timedelta(hours=24)),
+                        "last 7": (today - datetime.timedelta(days=7)),
+                        "last month": (today - datetime.timedelta(days=30)),
+                        "all logs": today}
+
 app.layout = html.Div([
-    dcc.Input(id='my-id', value='initial value', type='text'),
-    html.Div(id='my-div')
+    html.Div([
+        html.Div(id="my-div"),
+        dcc.Dropdown(
+            id='my-id',
+            options=[{'label': k, 'value': v} for i,(k, v) in enumerate(available_indicators.items())],
+            value= today
+        ),
+  ])
 ])
-
-
 @app.callback(
     Output(component_id='my-div', component_property='children'),
     [Input(component_id='my-id', component_property='value')]
