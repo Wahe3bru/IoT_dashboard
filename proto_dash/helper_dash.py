a11a5224c9ffcda67aa4e7ae47d703d6a078fe07
Wahe3bru <wahe3bru@gmail.com>
2019-04-05 14:40:35 +0200

    added methods to get all worksheet titles and return data into df

--- proto_dash/helper_dash.py
+++ proto_dash/helper_dash.py
@@ -1,11 +1,13 @@
 from os.path import join, dirname
 from dotenv import load_dotenv
+import pandas as pd
 import pygsheets
 
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
+client = pygsheets.authorize()
 
 def open_worksheet(spreadsheet_name, worksheet_title):
     """Open spreadsheet and worksheet, creating worksheet with column headers
@@ -30,6 +32,21 @@ def worksheet_as_df(spreadsheet_name, worksheet_title):
     df = wks.get_as_df()
     return df
 
+def get_worksheet_titles(spreadsheet_name):
+    sh = client.open(spreadsheet_name)
+    worksheet_titles = sh.worksheets()
+    return [str(title).split("'")[1] for title in worksheet_titles]
+
+def get_all_worksheets_as_df(spreadsheet_name):
+
+    worksheets = get_worksheet_titles(spreadsheet_name)
+
+    all_df = pd.DataFrame()
+    for title in worksheets:
+         df = worksheet_as_df(spreadsheet_name,title)
+         all_df = all_df.append(df)
+
+    return all_df
 
 def main():
     # testing
